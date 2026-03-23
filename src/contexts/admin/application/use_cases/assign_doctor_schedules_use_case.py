7d8342bd8625e3d2222d5d0f65b4a910d5d29d80
Johann Gaviria <johannmgr@gmail.com>
2026-03-23 18:13:35 -0500

    feat(admin): add cache invalidation when assigning doctor schedules

--- src/contexts/admin/application/use_cases/assign_doctor_schedules_use_case.py
+++ src/contexts/admin/application/use_cases/assign_doctor_schedules_use_case.py
@@ -12,10 +12,14 @@ from src.contexts.admin.domain.exceptions.exception import (
 from src.contexts.admin.domain.ports.repositories.doctor_schedules_repository_port import (
     DoctorSchedulesRepositoryPort,
 )
+from src.contexts.admin.domain.value_objects.appointment_availability_cache_key_vo import (
+    AppointmentAvailabilityCacheKeyVO,
+)
 from src.contexts.admin.domain.value_objects.timezone_vo import TimezoneVO
 from src.contexts.auth.domain.ports.repositories.doctor_repository_port import (
     DoctorRepositoryPort,
 )
+from src.shared.domain.ports.services.cache_service_port import CacheServicePort
 
 
 class AssignDoctorSchedulesUseCase:
@@ -25,6 +29,7 @@ class AssignDoctorSchedulesUseCase:
         self,
         doctor_repository_port: DoctorRepositoryPort,
         doctor_schedules_repository_port: DoctorSchedulesRepositoryPort,
+        cache_service_port: CacheServicePort,
     ):
         """Initialize the AssignDoctorSchedules use case with the required ports.
 
@@ -32,9 +37,11 @@ class AssignDoctorSchedulesUseCase:
             doctor_repository_port (DoctorRepositoryPort): Port for accessing doctor data.
             doctor_schedules_repository_port (DoctorSchedulesRepositoryPort):
                 Port for accessing doctor schedules data.
+            cache_service_port (CacheServicePort): Port for accessing cache services.
         """
         self.doctor_repository_port = doctor_repository_port
         self.doctor_schedules_repository_port = doctor_schedules_repository_port
+        self.cache_service_port = cache_service_port
 
     async def execute(self, command: AssignDoctorSchedulesCommand) -> None:
         """Execute the use case to assign doctor schedules.
@@ -72,3 +79,7 @@ class AssignDoctorSchedulesUseCase:
             timezone=TimezoneVO(command.timezone),
         )
         await self.doctor_schedules_repository_port.save(entity)
+
+        # Invalidate the cache fro the doctor's appointment availability
+        key = AppointmentAvailabilityCacheKeyVO.from_doctor_id(str(doctor.id))
+        self.cache_service_port.delete(key)
---
+++ src/contexts/admin/domain/value_objects/appointment_availability_cache_key_vo.py
@@ -0,0 +1,24 @@
+"""This module contains the value object for the appointment availability cache key."""
+
+from dataclasses import dataclass
+
+from src.shared.domain.value_objects.cache_key_vo import CacheKeyVO
+
+
+@dataclass(frozen=True)
+class AppointmentAvailabilityCacheKeyVO(CacheKeyVO):
+    """Value object representing a cache key for appointment availability in the admin context."""
+
+    @classmethod
+    def from_doctor_id(cls, doctor_id: str) -> "AppointmentAvailabilityCacheKeyVO":
+        """Create an instance of AppointmentAvailabilityCacheKeyVO from a doctor ID.
+
+        Args:
+            doctor_id (str): The ID of the doctor for which to create the cache key.
+
+        Returns:
+            AppointmentAvailabilityCacheKeyVO: An instance of the cache key value object
+                with the key formatted for appointment availability caching.
+        """
+        key = f"cache:admin:appointment_availability:{doctor_id}"
+        return cls(key=key)
